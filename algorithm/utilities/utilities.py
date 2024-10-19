ed3965a6465d2dbaa0ad416dde632c8066a2f506
molnardani2001 <dnai.molnar2001@gmail.com>
2024-10-19 16:30:56 +0200

    start dynamic algorithm of collision detection

--- algorithm/utilities/utilities.py
+++ algorithm/utilities/utilities.py
@@ -1,44 +1,19 @@
-import math
+class Vec:
+    def __init__(self, v, w):
+        self.v: tuple[int, int] = v
+        self.w: tuple[int, int] = w
+        self.multiset: dict[tuple[int,int], int] = {}
 
-from sortedcontainers import SortedList
+    def insert_vector(self, vector: tuple[int, int]):
+        if self.multiset.__contains__(vector):
+            self.multiset[vector] = self.multiset[vector] + 1
+        else:
+            self.multiset[vector] = 1
 
+    def get_vectors(self) -> list[tuple[int, int]]:
+        vectors: list[tuple[int, int]] = []
+        for e in self.multiset:
+            for i in range(0,self.multiset[e]):
+                vectors.append(e)
 
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y)
-
-    def angle(self):
-        return math.atan2(self.y, self.x)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __lt__(self, other):
-        return (self.x, self.y) < (other.x, other.y)
-
-    def __repr__(self):
-        return f"({self.x}, {self.y})"
-
-
-class VectorTree:
-    def __init__(self):
-        self.vectors = SortedList(key=lambda vec: vec.angle() )
-        self.sum_vector = Vector(0,0)
-
-    def insert_vector(self, vector):
-        self.vectors.add(vector)
-        self.sum_vector += vector
-
-    def remove_vector(self, vector):
-        self.vectors.remove(vector)
-        self.sum_vector -= vector
-
-    def get_sum_vector(self):
-        return self.sum_vector
+        return vectors
